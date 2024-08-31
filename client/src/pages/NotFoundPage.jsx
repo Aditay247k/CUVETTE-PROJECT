8d2fbd6eec6ea52805c2237fd3b9e9a5fc50fd2b
Aditay247k <aditayroy354@gmail.com>
2024-08-31 22:56:49 +0530

    imported multimedia assets

---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './NotFoundPage.module.css'
+
+ function NotFoundPage() {
+  return (
+    <div className={styles.container}>
+<h1>404 Page Not Found</h1>    
+<p>Sorry, the page you are looking for does not exist.</p>
+</div>
+  )
+}
+export default NotFoundPage;
